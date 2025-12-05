dc2bf41056ffbfa79d9655d66fe9d92d6e52358c
lisuyang <450028105@qq.com>
2025-12-05 16:35:27 +0800

    build(ui): 在vite配置中添加react/jsx-runtime为外部依赖

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
       fileName: (format) => `index.${format}.js`
     },
     rollupOptions: {
-      external: ['react', 'react-dom']
-    }
-  }
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
+    }, 
+  }, 
 });
